1aca63b14132eb14be30f0dd983e8b9ca59793fd
abhishek <abhishekyadavcs01@gmail.com>
2023-02-15 18:31:28 +0530

    useState hooks changed to useReducer hook

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,33 +1,29 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useContext } from 'react'
+
 import Style from '../components/Button.module.css'
+import { userContext } from '../App'
 
 
-const Title = (props) => {
+const Title = () => {
+  const appContext = useContext(userContext)
+  const{state,dispatch}= appContext
 
-  const{title,setTitle,des,setDes} = props
   
-  const eventhandler = (e) => {
-    setTitle(e.target.value)
-
-  }
-  const handler = (e) => {
-    setDes(e.target.value)
-
-  }
+  
   return (
     <div className={Style["spacing"]}>
       <div className="mb-3">
         <label htmlFor="exampleFormControlInput1" className="form-label" >Title</label>
-        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="The first task Title" onChange={eventhandler} value={title} />
+        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="The first task Title" onChange={(e)=>dispatch({type:"title",data:e.target.value})} />
       </div>
       <div className="mb-3">
         <label htmlFor="exampleFormControlTextarea1" className="form-label">Description</label>
-        <textarea className="form-control" id="exampleFormControlTextarea1" placeholder='Add Description Here ' rows="3" onChange={handler} value={des}></textarea>
+        <textarea className="form-control" id="exampleFormControlTextarea1" placeholder='Add Description Here ' rows="3" onChange={(e)=>dispatch({type:"description", data:e.target.value}) }></textarea>
       </div>
 
     </div>
   )
 }
+
 
 export default Title
