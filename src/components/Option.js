0f82859ed1ff93fe905699ceb52854ce386b7a8d
abhishek <abhishekyadavcs01@gmail.com>
2023-02-17 17:43:17 +0530

    mapping of an Component

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import './Option.css'
+import React,{useContext} from 'react';
+import { userContext } from '../App';
+import './Option.css';
+import Navbar from './Navbar';
+import Title from './Title';
+import Buttons from './Buttons';
 
 const Option = () => {
+    const setContext =useContext(userContext)
+    const{state,dispatch} =setContext
     return (
+        <div>
+      
+      {/* {setContext.optionedit && <><div className="container">
+
+        <div className='app-wrapper '>
+          <Navbar />
+          <Title/>
+
+          <Buttons/>
+      
+
+        </div> */}
+        
+
+
+      {/* </div>
+
+      </>} */}
         <div className='e1' style={{ position: "absolute",  zIndex: 11111 }}>
-            <button id="t1"  className='btn btn-light'>Edit...</button>
-            <button id="t2" className='btn btn-light'>Delete</button>
+            <button id="t1"  className='btn btn-light' onClick={()=>dispatch({type:'optionedit'})}>Edit...</button>
+            <button id="t2" className='btn btn-light' onClick={()=>dispatch({type:'optiondelete'})}>Delete</button>
+        </div>
         </div>
     )
 }
