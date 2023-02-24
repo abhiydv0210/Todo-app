84cf941435d435ff5c6b329721092a81aaf6e06c
abhishek <abhishekyadavcs01@gmail.com>
2023-02-24 18:50:12 +0530

    worked on Edit Button

--- src/components/Title.js
+++ src/components/Title.js
@@ -14,11 +14,11 @@ const Title = () => {
     <div className={Style["spacing"]}>
       <div className="mb-3">
         <label htmlFor="exampleFormControlInput1" className="form-label" >Title</label>
-        <input type="text"  className="form-control" id="exampleFormControlInput1" placeholder="The first task Title" onChange={(e)=>dispatch({type:"title",data:e.target.value})} />
+        <input type="text"  className="form-control" id="exampleFormControlInput1" placeholder="The first task Title" value={setContext.title} onChange={(e)=>dispatch({type:"title",id:e.target.value})} />
       </div>
       <div className="mb-3">
         <label htmlFor="exampleFormControlTextarea1" className="form-label">Description</label>
-        <textarea className="form-control" id="exampleFormControlTextarea1" placeholder='Add Description Here ' rows="3" onChange={(e)=>dispatch({type:"description", data:e.target.value}) }></textarea>
+        <textarea className="form-control" id="exampleFormControlTextarea1" placeholder='Add Description Here ' rows="4" value={setContext.description} onChange={(e)=>dispatch({type:"description", id:e.target.value}) }></textarea>
       </div>
 
     </div>
