086860eb17b17dd6ed8a4b7ef19aefa282a85fd9
abhishek <abhishekyadavcs01@gmail.com>
2023-03-22 19:12:53 +0530

    Worked on Edit ,Upadte & alltodo  on API in Todo app

--- src/components/ LoginPage.js
+++ src/components/ LoginPage.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import './LoginPage.css';
 import { userContext } from '../App';
 import useFetchAPI from './FetchAPI';
@@ -6,71 +6,78 @@ import useFetchAPI from './FetchAPI';
 
 function LoginPage() {
   const setContext = useContext(userContext)
-  const {dispatch } = setContext;
+  const { dispatch } = setContext;
   const [log, setLog] = useState({})
   const api = useFetchAPI();
-  
-  const validate = (data) =>{
-    if(!log["email"] )
-    return false;
-    if(!log["password"]  )
-    return false;
 
-    return true ; 
+  const validate = (data) => {
+    if (!log["email"])
+      return false;
+    if (!log["password"])
+      return false;
+
+    return true;
   }
   const login = () => {
-    console.log(log,"---")
+    console.log(log, "---")
     const result = validate(log)
     console.log("adsfasfs")
-    if(!result){
+    if (!result) {
       alert("please fill all the filed correctly")
       return
     }
-    api("user/login", "POST", log, (res, error)=>{
-      if(error){
+    api("user/login", "POST", log, (res, error) => {
+      if (error) {
         alert(error)
         console.log(error, "error from api")
-      }else{
+      }
+      else {
         localStorage.setItem("token", res.access_token)
+        dispatch({type:"setUserLogin", status: true})
         console.log(res, "---->")
       }
-    })
-    }
-   
+    });
+    dispatch({ type: "login" })
+
+  }
+
+
+
+
   return (
-    <form onSubmit={e=>e.preventDefault()}>
-    
-    <div className='app-wrappe' id='main'>
-      
-      
-      <div id="dis" >
-        <h3 >log in</h3>
-      
-      <h3><i id='mult' class="fa-sharp fa-solid fa-xmark" onClick={() => dispatch({ type: 'Loginclose'})} ></i></h3>
-      </div>
+    <form onSubmit={e => e.preventDefault()}>
+
+      <div className='app-wrappe' id='main'>
+
+
+        <div id="dis" >
+          <h3 >log in</h3>
+
+          <h3><i id='mult' class="fa-sharp fa-solid fa-xmark" onClick={() => dispatch({ type: 'Loginclose' })} ></i></h3>
+        </div>
         <div class="mb-0">
           <label for="exampleInputEmail1" class="form-label">Email</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required   value={log["email"]} onChange={(e) => setLog(p => ({ ...p, email: e.target.value }))}/>
-            
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required value={log["email"]} onChange={(e) => setLog(p => ({ ...p, email: e.target.value }))} />
+
         </div>
 
         <div class="mb-0">
           <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" value={log["password"]} required 
-             onChange={(e) => setLog(p => ({ ...p, password: e.target.value }))}/>
+          <input type="password" class="form-control" id="exampleInputPassword1" value={log["password"]} required
+            onChange={(e) => setLog(p => ({ ...p, password: e.target.value }))} />
         </div>
         <br></br>
-        
+
         <button id='wid' type="login" class="btn btn-danger" onClick={login}>login</button>
-        <h6 id='back' onClick={() => dispatch({ type: 'Forgetpassword'})}>Forget Password ?</h6>
+        <h6 id='back' onClick={() => dispatch({ type: 'Forgetpassword' })}>Forget Password ?</h6>
         <hr></hr>
         <p id='acc'>Already have an account ?</p>
-        
-        <p id='sign' onClick={() => dispatch({ type: 'Signupopen'})}>SignUp</p>
 
-      
+        <p id='sign' onClick={() => dispatch({ type: 'Signupopen' })}>SignUp</p>
 
-    </div>
+
+
+      </div>
     </form>
   )
 }
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,19 +1,41 @@
-import React,{useContext} from 'react';
+import React,{useContext,useState} from 'react';
 import { userContext } from '../App';
+import useFetchAPI from './FetchAPI';
 import './Option.css';
 
 
 const Option = ({id}) => {
     const setContext =useContext(userContext)
-    const{dispatch} =setContext
-    console.log(id,"hey i m option")
+    const{dispatch} =setContext;
+    // const [del,setDel] = useState({})
+    const api = useFetchAPI();
+
+
+    const Deleted = () => {
+        let data ={_id:id}
+        
+        api("user/deleteTodo", "DELETE",data, (res, error)=>{
+          if(error){
+            alert(error)
+            console.log(error, "error from api")
+          }if(res){
+            dispatch({ type: "optiondelete", id })
+            console.log(res, "---->")
+          }
+        })
+       
+    
+        
+      }
+    
+    // console.log(id,"hey i m option")
     return (
         <div className='e2' >
       
       
         <div className='e1' style={{ position: "absolute"}}>
             <button id="t1"  className='btn btn-light' onClick={()=>dispatch({type:'optionedit',id})}>Edit...</button>
-            <button id="t2" className='btn btn-light' onClick={()=>dispatch({type:'optiondelete', id})}>Delete</button>
+            <button id="t2" className='btn btn-light' onClick={Deleted}>Delete</button>
         </div>
         
 
